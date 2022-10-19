6c576d69136256951b5d1d00d84261a7de8d33ea
James Gaddum <james@calculated.fi>
2022-10-19 16:51:43 +1300

    remove hardcoded value for target price

--- contracts/dca/src/handlers/fin_limit_order_submitted.rs
+++ contracts/dca/src/handlers/fin_limit_order_submitted.rs
@@ -1,8 +1,7 @@
 use crate::error::ContractError;
-use crate::state::{save_trigger, CACHE};
+use crate::state::{get_trigger, remove_trigger, save_trigger, CACHE};
 use base::helpers::message_helpers::get_flat_map_for_event_type;
 use base::triggers::trigger::{Trigger, TriggerConfiguration};
-use cosmwasm_std::Decimal256;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::{DepsMut, Reply, Response, Uint128};
 use std::str::FromStr;
@@ -20,16 +19,26 @@ pub fn fin_limit_order_submitted(deps: DepsMut, reply: Reply) -> Result<Response
 
             let cache = CACHE.load(deps.storage)?;
 
-            save_trigger(
-                deps.storage,
-                Trigger {
-                    vault_id: cache.vault_id,
-                    configuration: TriggerConfiguration::FINLimitOrder {
-                        order_idx: Some(order_idx),
-                        target_price: Decimal256::one(),
-                    },
-                },
-            )?;
+            let trigger = get_trigger(deps.storage, cache.vault_id)?;
+            remove_trigger(deps.storage, cache.vault_id)?;
+
+            match trigger.configuration {
+                TriggerConfiguration::FINLimitOrder { target_price, .. } => {
+                    save_trigger(
+                        deps.storage,
+                        Trigger {
+                            vault_id: cache.vault_id,
+                            configuration: TriggerConfiguration::FINLimitOrder {
+                                order_idx: Some(order_idx),
+                                target_price,
+                            },
+                        },
+                    )?;
+                }
+                TriggerConfiguration::Time { .. } => {
+                    panic!("should be a fin limit order trigger")
+                }
+            }
 
             CACHE.remove(deps.storage);
 
