d9158da314ecea166ee8b9e2540ab158021f6072
aidan <aidan@calculated.fi>
2022-10-21 11:51:54 +1100

    upgrade fin helpers

--- packages/fin-helpers/src/msg.rs
+++ packages/fin-helpers/src/msg.rs
@@ -1,19 +1,19 @@
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Decimal256, Uint128, Uint256};
-use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[cw_serde]
 pub struct FINPoolResponseWithoutDenom {
     pub quote_price: Decimal256,
     pub total_offer_amount: Uint256,
 }
 
-#[derive(Deserialize)]
+#[cw_serde]
 pub struct FINBookResponse {
     pub base: Vec<FINPoolResponseWithoutDenom>,
     pub quote: Vec<FINPoolResponseWithoutDenom>,
 }
 
-#[derive(Deserialize)]
+#[cw_serde]
 pub struct FINOrderResponseWithoutDenom {
     pub offer_amount: Uint128,
     pub filled_amount: Uint128,
