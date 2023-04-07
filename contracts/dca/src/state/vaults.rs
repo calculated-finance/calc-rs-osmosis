85743f602b7164a090e431bedebd4fb7be766480
James Gaddum <james@calculated.fi>
2023-04-07 14:57:01 +1200

    simplify vault storage

--- contracts/dca/src/state/vaults.rs
+++ contracts/dca/src/state/vaults.rs
@@ -1,118 +1,15 @@
 use super::{pairs::PAIRS, state_helpers::fetch_and_increment_counter, triggers::get_trigger};
 use crate::types::{
     dca_plus_config::DcaPlusConfig,
-    destination::Destination,
-    pair::Pair,
-    price_delta_limit::PriceDeltaLimit,
     time_interval::TimeInterval,
-    trigger::TriggerConfiguration,
     vault::{Vault, VaultStatus},
     vault_builder::VaultBuilder,
 };
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{
-    from_binary, to_binary, Addr, Binary, Coin, Decimal, StdResult, Storage, Timestamp, Uint128,
-};
-use cw_storage_plus::{Bound, Index, IndexList, IndexedMap, Item, Map, UniqueIndex};
+use cosmwasm_std::{Addr, Binary, Coin, Decimal, StdResult, Storage, Timestamp, Uint128};
+use cw_storage_plus::{Bound, Index, IndexList, IndexedMap, Item, UniqueIndex};
 
 const VAULT_COUNTER: Item<u64> = Item::new("vault_counter_v3");
-
-#[cw_serde]
-struct VaultData {
-    pub id: Uint128,
-    pub created_at: Timestamp,
-    pub owner: Addr,
-    pub label: Option<String>,
-    pub destinations: Vec<Destination>,
-    pub status: VaultStatus,
-    pub balance: Coin,
-    pub pair_address: Addr,
-    pub swap_amount: Uint128,
-    pub slippage_tolerance: Option<Decimal>,
-    pub minimum_receive_amount: Option<Uint128>,
-    pub time_interval: TimeInterval,
-    pub started_at: Option<Timestamp>,
-    pub swapped_amount: Coin,
-    pub received_amount: Coin,
-    pub price_delta_limits: Vec<PriceDeltaLimit>,
-}
-
-impl From<Vault> for VaultData {
-    fn from(vault: Vault) -> Self {
-        Self {
-            id: vault.id,
-            created_at: vault.created_at,
-            owner: vault.owner,
-            label: vault.label,
-            destinations: vec![],
-            status: vault.status,
-            balance: vault.balance,
-            pair_address: vault.pair.address,
-            swap_amount: vault.swap_amount,
-            slippage_tolerance: vault.slippage_tolerance,
-            minimum_receive_amount: vault.minimum_receive_amount,
-            time_interval: vault.time_interval,
-            started_at: vault.started_at,
-            swapped_amount: vault.swapped_amount,
-            received_amount: vault.received_amount,
-            price_delta_limits: vec![],
-        }
-    }
-}
-
-fn vault_from(
-    data: &VaultData,
-    pair: Pair,
-    trigger: Option<TriggerConfiguration>,
-    destinations: &mut Vec<Destination>,
-    dca_plus_config: Option<DcaPlusConfig>,
-) -> Vault {
-    destinations.append(
-        &mut data
-            .destinations
-            .clone()
-            .into_iter()
-            .map(|destination| destination.into())
-            .collect(),
-    );
-    Vault {
-        id: data.id,
-        created_at: data.created_at,
-        owner: data.owner.clone(),
-        label: data.label.clone(),
-        destinations: destinations.clone(),
-        status: data.status.clone(),
-        balance: data.balance.clone(),
-        pair,
-        swap_amount: data.swap_amount,
-        slippage_tolerance: data.slippage_tolerance,
-        minimum_receive_amount: data.minimum_receive_amount,
-        time_interval: data.time_interval.clone(),
-        started_at: data.started_at,
-        swapped_amount: data.swapped_amount.clone(),
-        received_amount: data.received_amount.clone(),
-        trigger,
-        dca_plus_config,
-    }
-}
-
-const DESTINATIONS: Map<u128, Binary> = Map::new("destinations_v3");
-
-fn get_destinations(store: &dyn Storage, vault_id: Uint128) -> StdResult<Vec<Destination>> {
-    let destinations = DESTINATIONS.may_load(store, vault_id.into())?;
-    match destinations {
-        Some(destinations) => Ok(from_binary(&destinations)?),
-        None => Ok(vec![]),
-    }
-}
-
-const DCA_PLUS_CONFIGS: Map<u128, DcaPlusConfig> = Map::new("dca_plus_configs_v3");
-
-fn get_dca_plus_config(store: &dyn Storage, vault_id: Uint128) -> Option<DcaPlusConfig> {
-    DCA_PLUS_CONFIGS
-        .may_load(store, vault_id.into())
-        .unwrap_or(None)
-}
 
 struct VaultIndexes<'a> {
     pub owner: UniqueIndex<'a, (Addr, u128), VaultData, u128>,
@@ -139,27 +36,13 @@ fn vault_store<'a>() -> IndexedMap<'a, u128, VaultData, VaultIndexes<'a>> {
 
 pub fn save_vault(store: &mut dyn Storage, vault_builder: VaultBuilder) -> StdResult<Vault> {
     let vault = vault_builder.build(fetch_and_increment_counter(store, VAULT_COUNTER)?.into());
-    DESTINATIONS.save(
-        store,
-        vault.id.into(),
-        &to_binary(&vault.destinations).expect("serialised destinations"),
-    )?;
-    if let Some(dca_plus_config) = vault.dca_plus_config.clone() {
-        DCA_PLUS_CONFIGS.save(store, vault.id.into(), &dca_plus_config)?;
-    }
     vault_store().save(store, vault.id.into(), &vault.clone().into())?;
     Ok(vault)
 }
 
 pub fn get_vault(store: &dyn Storage, vault_id: Uint128) -> StdResult<Vault> {
-    let data = vault_store().load(store, vault_id.into())?;
-    Ok(vault_from(
-        &data,
-        PAIRS.load(store, data.pair_address.clone())?,
-        get_trigger(store, vault_id)?.map(|t| t.configuration),
-        &mut get_destinations(store, vault_id)?,
-        get_dca_plus_config(store, vault_id),
-    ))
+    let vault_data = vault_store().load(store, vault_id.into())?;
+    Ok(vault_from(store, &vault_data))
 }
 
 pub fn get_vaults_by_address(
@@ -186,19 +69,8 @@ pub fn get_vaults_by_address(
         )
         .take(limit.unwrap_or(30) as usize)
         .map(|result| {
-            let (_, vault_data) =
-                result.expect(format!("a vault with id after {:?}", start_after).as_str());
-            vault_from(
-                &vault_data,
-                PAIRS.load(store, vault_data.pair_address.clone()).expect(
-                    format!("a pair for pair address {:?}", vault_data.pair_address).as_str(),
-                ),
-                get_trigger(store, vault_data.id.into())
-                    .expect(format!("a trigger for vault id {}", vault_data.id).as_str())
-                    .map(|trigger| trigger.configuration),
-                &mut get_destinations(store, vault_data.id).expect("vault destinations"),
-                get_dca_plus_config(store, vault_data.id),
-            )
+            let (_, vault_data) = result.expect(format!("vault after {:?}", start_after).as_str());
+            vault_from(store, &vault_data)
         })
         .collect::<Vec<Vault>>())
 }
@@ -217,271 +89,90 @@ pub fn get_vaults(
         )
         .take(limit.unwrap_or(30) as usize)
         .map(|result| {
-            let (_, vault_data) =
-                result.expect(format!("a vault with id after {:?}", start_after).as_str());
-            vault_from(
-                &vault_data,
-                PAIRS.load(store, vault_data.pair_address.clone()).expect(
-                    format!("a pair for pair address {:?}", vault_data.pair_address).as_str(),
-                ),
-                get_trigger(store, vault_data.id.into())
-                    .expect(format!("a trigger for vault id {}", vault_data.id).as_str())
-                    .map(|trigger| trigger.configuration),
-                &mut get_destinations(store, vault_data.id).expect("vault destinations"),
-                get_dca_plus_config(store, vault_data.id),
-            )
+            let (_, vault_data) = result.expect(format!("vault after {:?}", start_after).as_str());
+            vault_from(store, &vault_data)
         })
         .collect::<Vec<Vault>>())
 }
 
 pub fn update_vault(store: &mut dyn Storage, vault: &Vault) -> StdResult<()> {
-    DESTINATIONS.save(
-        store,
-        vault.id.into(),
-        &to_binary(&vault.destinations).expect("serialised destinations"),
-    )?;
-    if let Some(dca_plus_config) = &vault.dca_plus_config {
-        DCA_PLUS_CONFIGS.save(store, vault.id.into(), dca_plus_config)?;
-    }
     vault_store().save(store, vault.id.into(), &vault.clone().into())
 }
 
-pub fn clear_vaults(store: &mut dyn Storage) {
-    vault_store().clear(store);
-    VAULT_COUNTER.remove(store)
+#[cw_serde]
+struct VaultData {
+    pub id: Uint128,
+    pub created_at: Timestamp,
+    pub owner: Addr,
+    pub label: Option<String>,
+    pub destinations: Vec<Binary>, // store destinations as binary to allow post execution action extension
+    pub status: VaultStatus,
+    pub balance: Coin,
+    pub pair_address: Addr,
+    pub swap_amount: Uint128,
+    pub slippage_tolerance: Option<Decimal>,
+    pub minimum_receive_amount: Option<Uint128>,
+    pub time_interval: TimeInterval,
+    pub started_at: Option<Timestamp>,
+    pub swapped_amount: Coin,
+    pub received_amount: Coin,
+    pub dca_plus_config: Option<DcaPlusConfig>,
 }
 
-#[cfg(test)]
-mod destination_store_tests {
-    use super::*;
-    use crate::{
-        tests::mocks::{DENOM_STAKE, DENOM_UOSMO},
-        types::{post_execution_action::PostExecutionAction, vault_builder::VaultBuilder},
-    };
-    use cosmwasm_std::{
-        testing::{mock_dependencies, mock_env},
-        Addr, Coin, Decimal, Env, Uint128,
-    };
-
-    fn create_vault_builder(env: Env) -> VaultBuilder {
-        VaultBuilder::new(
-            env.block.time,
-            Addr::unchecked("owner"),
-            None,
-            vec![Destination {
-                address: Addr::unchecked("owner"),
-                allocation: Decimal::one(),
-                action: PostExecutionAction::Send,
-            }],
-            VaultStatus::Active,
-            Coin::new(1000u128, DENOM_UOSMO.to_string()),
-            Pair {
-                pool_id: 0,
-                address: Addr::unchecked("pair"),
-                base_denom: DENOM_STAKE.to_string(),
-                quote_denom: DENOM_UOSMO.to_string(),
-            },
-            Uint128::new(100),
-            None,
-            None,
-            None,
-            TimeInterval::Daily,
-            None,
-            Coin {
-                denom: DENOM_STAKE.to_string(),
-                amount: Uint128::zero(),
-            },
-            Coin {
-                denom: DENOM_UOSMO.to_string(),
-                amount: Uint128::zero(),
-            },
-            None,
-        )
+impl From<Vault> for VaultData {
+    fn from(vault: Vault) -> Self {
+        Self {
+            id: vault.id,
+            created_at: vault.created_at,
+            owner: vault.owner,
+            label: vault.label,
+            status: vault.status,
+            balance: vault.balance,
+            swap_amount: vault.swap_amount,
+            slippage_tolerance: vault.slippage_tolerance,
+            minimum_receive_amount: vault.minimum_receive_amount,
+            time_interval: vault.time_interval,
+            started_at: vault.started_at,
+            swapped_amount: vault.swapped_amount,
+            received_amount: vault.received_amount,
+            dca_plus_config: vault.dca_plus_config,
+            destinations: vault.destinations.into_iter().map(|d| d.into()).collect(),
+            pair_address: vault.pair.address,
+        }
     }
+}
 
-    #[test]
-    fn saving_new_vault_stores_destinations() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let store = deps.as_mut().storage;
+fn vault_from(store: &dyn Storage, data: &VaultData) -> Vault {
+    let pair = PAIRS
+        .load(store, data.pair_address.clone())
+        .expect(format!("pair for pair address {:?}", data.pair_address).as_str());
 
-        let vault_builder = create_vault_builder(env);
-        let vault = save_vault(store, vault_builder).unwrap();
+    let trigger = get_trigger(store, data.id.into())
+        .expect(format!("trigger for vault id {}", data.id).as_str())
+        .map(|t| t.configuration);
 
-        let destinations: Vec<Destination> =
-            from_binary(&DESTINATIONS.load(store, vault.id.into()).unwrap()).unwrap();
-        assert_eq!(destinations, vault.destinations);
-        assert!(!destinations.is_empty());
-    }
-
-    #[test]
-    fn fetching_new_vault_returns_destinations() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let store = deps.as_mut().storage;
-
-        let pair = Pair {
-            pool_id: 0,
-            address: Addr::unchecked("pair"),
-            base_denom: DENOM_STAKE.to_string(),
-            quote_denom: DENOM_UOSMO.to_string(),
-        };
-
-        PAIRS
-            .save(store, pair.address.clone(), &pair.clone())
-            .unwrap();
-
-        let vault_builder = create_vault_builder(env);
-        let vault = save_vault(store, vault_builder).unwrap();
-        let fetched_vault = get_vault(store, vault.id).unwrap();
-
-        assert_eq!(fetched_vault.destinations, vault.destinations);
-        assert!(!fetched_vault.destinations.is_empty());
-    }
-
-    #[test]
-    fn fetching_new_vault_after_update_returns_destinations() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let store = deps.as_mut().storage;
-
-        let pair = Pair {
-            pool_id: 0,
-            address: Addr::unchecked("pair"),
-            base_denom: DENOM_STAKE.to_string(),
-            quote_denom: DENOM_UOSMO.to_string(),
-        };
-
-        PAIRS
-            .save(store, pair.address.clone(), &pair.clone())
-            .unwrap();
-
-        let vault_builder = create_vault_builder(env);
-        let mut vault = save_vault(store, vault_builder).unwrap();
-
-        vault.status = VaultStatus::Inactive;
-        update_vault(store, &vault).unwrap();
-
-        let fetched_vault = get_vault(store, vault.id).unwrap();
-        assert_eq!(fetched_vault.destinations, vault.destinations);
-        assert!(!fetched_vault.destinations.is_empty());
-    }
-
-    #[test]
-    fn fetching_old_vault_returns_destinations() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let store = deps.as_mut().storage;
-
-        let pair = Pair {
-            pool_id: 0,
-            address: Addr::unchecked("pair"),
-            base_denom: DENOM_STAKE.to_string(),
-            quote_denom: DENOM_UOSMO.to_string(),
-        };
-
-        PAIRS
-            .save(store, pair.address.clone(), &pair.clone())
-            .unwrap();
-
-        let vault = create_vault_builder(env).build(Uint128::one());
-
-        let mut vault_dto: VaultData = vault.clone().into();
-        vault_dto.destinations = vault
-            .clone()
+    Vault {
+        id: data.id,
+        created_at: data.created_at,
+        owner: data.owner.clone(),
+        label: data.label.clone(),
+        status: data.status.clone(),
+        balance: data.balance.clone(),
+        swap_amount: data.swap_amount,
+        slippage_tolerance: data.slippage_tolerance,
+        minimum_receive_amount: data.minimum_receive_amount,
+        time_interval: data.time_interval.clone(),
+        started_at: data.started_at,
+        swapped_amount: data.swapped_amount.clone(),
+        received_amount: data.received_amount.clone(),
+        dca_plus_config: data.dca_plus_config.clone(),
+        destinations: data
             .destinations
             .clone()
             .into_iter()
-            .map(|d| d.into())
-            .collect();
-
-        vault_store()
-            .save(store, vault.id.into(), &vault_dto)
-            .unwrap();
-
-        let fetched_vault = get_vault(store, vault.id).unwrap();
-        assert_eq!(fetched_vault.destinations, vault.destinations);
-        assert!(!fetched_vault.destinations.is_empty());
-    }
-
-    #[test]
-    fn updating_old_vault_stores_destinations() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let store = deps.as_mut().storage;
-
-        let pair = Pair {
-            pool_id: 0,
-            address: Addr::unchecked("pair"),
-            base_denom: DENOM_STAKE.to_string(),
-            quote_denom: DENOM_UOSMO.to_string(),
-        };
-
-        PAIRS
-            .save(store, pair.address.clone(), &pair.clone())
-            .unwrap();
-
-        let mut vault = create_vault_builder(env).build(Uint128::one());
-
-        let mut vault_dto: VaultData = vault.clone().into();
-        vault_dto.destinations = vault
-            .clone()
-            .destinations
-            .clone()
-            .into_iter()
-            .map(|d| d.into())
-            .collect();
-
-        vault_store()
-            .save(store, vault.id.into(), &vault_dto)
-            .unwrap();
-
-        vault.status = VaultStatus::Inactive;
-        update_vault(store, &vault).unwrap();
-
-        let destinations: Vec<Destination> =
-            from_binary(&DESTINATIONS.load(store, vault.id.into()).unwrap()).unwrap();
-        assert_eq!(destinations, vault.destinations);
-        assert!(!destinations.is_empty());
-    }
-
-    #[test]
-    fn fetching_old_vault_after_update_returns_destinations() {
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let store = deps.as_mut().storage;
-
-        let pair = Pair {
-            pool_id: 0,
-            address: Addr::unchecked("pair"),
-            base_denom: DENOM_STAKE.to_string(),
-            quote_denom: DENOM_UOSMO.to_string(),
-        };
-
-        PAIRS
-            .save(store, pair.address.clone(), &pair.clone())
-            .unwrap();
-
-        let mut vault = create_vault_builder(env).build(Uint128::one());
-
-        let mut vault_dto: VaultData = vault.clone().into();
-        vault_dto.destinations = vault
-            .clone()
-            .destinations
-            .clone()
-            .into_iter()
-            .map(|d| d.into())
-            .collect();
-
-        vault_store()
-            .save(store, vault.id.into(), &vault_dto)
-            .unwrap();
-
-        vault.status = VaultStatus::Inactive;
-        update_vault(store, &vault).unwrap();
-
-        let fetched_vault = get_vault(store, vault.id).unwrap();
-        assert_eq!(fetched_vault.destinations, vault.destinations);
-        assert!(!fetched_vault.destinations.is_empty());
+            .map(|b| b.into())
+            .collect(),
+        pair,
+        trigger,
     }
 }
--- contracts/dca/src/types/destination.rs
+++ contracts/dca/src/types/destination.rs
@@ -1,10 +1,22 @@
 use super::post_execution_action::PostExecutionAction;
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Addr, Decimal};
+use cosmwasm_std::{from_binary, to_binary, Addr, Binary, Decimal};
 
 #[cw_serde]
 pub struct Destination {
     pub address: Addr,
     pub allocation: Decimal,
     pub action: PostExecutionAction,
+}
+
+impl From<Destination> for Binary {
+    fn from(destination: Destination) -> Self {
+        to_binary(&destination).expect(&format!("serialised destination {:#?}", destination))
+    }
+}
+
+impl From<Binary> for Destination {
+    fn from(binary: Binary) -> Self {
+        from_binary(&binary).expect(&format!("deserialised destination {:#?}", binary))
+    }
 }
