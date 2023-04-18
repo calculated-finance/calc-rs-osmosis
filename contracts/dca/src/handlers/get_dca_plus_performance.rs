b578574ed02169ae5ad7da0f384b8f7626a25de8
James Gaddum <james@calculated.fi>
2023-04-18 16:14:02 +1200

    add tests for dca+ performance

--- contracts/dca/src/handlers/get_dca_plus_performance.rs
+++ contracts/dca/src/handlers/get_dca_plus_performance.rs
@@ -28,3 +28,70 @@ pub fn get_dca_plus_performance_handler(
         },
     )
 }
+
+#[cfg(test)]
+mod get_dca_plus_performance_tests {
+    use super::get_dca_plus_performance_handler;
+    use crate::{
+        constants::{ONE, TEN},
+        tests::{
+            helpers::setup_new_vault,
+            mocks::{calc_mock_dependencies, DENOM_STAKE},
+        },
+        types::{dca_plus_config::DcaPlusConfig, vault::Vault},
+    };
+    use cosmwasm_std::{testing::mock_env, Coin, Decimal};
+
+    #[test]
+    fn if_not_a_dca_plus_vault_fails() {
+        let mut deps = calc_mock_dependencies();
+        let env = mock_env();
+
+        let vault = setup_new_vault(deps.as_mut(), env, Vault::default());
+
+        let err = get_dca_plus_performance_handler(deps.as_ref(), vault.id).unwrap_err();
+
+        assert_eq!(
+            err.to_string(),
+            "Generic error: Vault 0 is not a DCA Plus vault"
+        );
+    }
+
+    #[test]
+    fn performance_fee_and_factor_match() {
+        let mut deps = calc_mock_dependencies();
+        let env = mock_env();
+
+        let standard_received_amount = TEN - ONE;
+
+        let dca_plus_config = DcaPlusConfig {
+            standard_dca_swapped_amount: Coin::new(TEN.into(), DENOM_STAKE),
+            standard_dca_received_amount: Coin::new(standard_received_amount.into(), DENOM_STAKE),
+            escrowed_balance: Coin::new(TEN.into(), DENOM_STAKE),
+            ..DcaPlusConfig::default()
+        };
+
+        let vault = setup_new_vault(
+            deps.as_mut(),
+            env,
+            Vault {
+                swapped_amount: Coin::new(TEN.into(), DENOM_STAKE),
+                received_amount: Coin::new(TEN.into(), DENOM_STAKE),
+                dca_plus_config: Some(dca_plus_config.clone()),
+                ..Vault::default()
+            },
+        );
+
+        let response = get_dca_plus_performance_handler(deps.as_ref(), vault.id).unwrap();
+
+        assert_eq!(
+            response.fee,
+            Coin::new(
+                ((standard_received_amount * response.factor - standard_received_amount)
+                    * Decimal::percent(20))
+                .into(),
+                DENOM_STAKE
+            )
+        );
+    }
+}
