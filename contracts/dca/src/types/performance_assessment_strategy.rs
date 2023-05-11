01303745942e717507270e8d8b2261b8354e4d3b
jamesgaddumcalc <112296686+jamesgaddumcalc@users.noreply.github.com>
2023-05-11 18:38:04 +1200

    refactor should_continue method into performance assessment strategy (#78)

--- contracts/dca/src/types/performance_assessment_strategy.rs
+++ contracts/dca/src/types/performance_assessment_strategy.rs
@@ -1,6 +1,8 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::Coin;
 
+use super::vault::Vault;
+
 #[cw_serde]
 pub enum PerformanceAssessmentStrategy {
     CompareToStandardDca {
@@ -15,30 +17,11 @@ pub enum PerformanceAssessmentStrategyParams {
 }
 
 impl PerformanceAssessmentStrategy {
-    pub fn standard_dca_swapped_amount(&self) -> Coin {
+    pub fn should_continue(&self, vault: &Vault) -> bool {
         match self {
             PerformanceAssessmentStrategy::CompareToStandardDca { swapped_amount, .. } => {
-                swapped_amount.clone()
+                vault.deposited_amount.amount > swapped_amount.amount
             }
         }
-    }
-
-    pub fn standard_dca_received_amount(&self) -> Coin {
-        match self {
-            PerformanceAssessmentStrategy::CompareToStandardDca {
-                received_amount, ..
-            } => received_amount.clone(),
-        }
-    }
-
-    pub fn standard_dca_balance(self, deposited_amount: Coin) -> Coin {
-        if self.standard_dca_swapped_amount().amount >= deposited_amount.amount {
-            return Coin::new(0, self.standard_dca_swapped_amount().denom);
-        }
-
-        Coin::new(
-            (deposited_amount.amount - self.standard_dca_swapped_amount().amount).into(),
-            self.standard_dca_swapped_amount().denom,
-        )
     }
 }
